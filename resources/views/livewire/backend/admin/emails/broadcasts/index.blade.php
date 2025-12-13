e0974257abd4aff60e152df59212f38bf279fbbd
Tom Harley <tomharley@macbookair.home>
2025-12-13 15:22:57 +0000

    updated to category label

--- resources/views/livewire/backend/admin/emails/broadcasts/index.blade.php
+++ resources/views/livewire/backend/admin/emails/broadcasts/index.blade.php
@@ -84,7 +84,7 @@
                     <tr class="border-b border-[var(--color-border)] hover:bg-[var(--color-surface-hover)] transition">
 
                         <td class="px-4 py-3">
-                            {{ $broadcast->type->label }}<br>
+                            {{ $broadcast->type->category->label }}<br>
                             <span class="text-xs text-[var(--color-text)]/40">
                                 {{ $broadcast->queued_at->diffForHumans() }}
                             </span>
