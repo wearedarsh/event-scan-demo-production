c3c4b3313d2cfff7008123fbc242c2dea7cbadc4
Tom Harley <tomharley@macbookair.home>
2025-12-13 09:37:44 +0000

    updated broadcasts index

--- resources/views/livewire/backend/admin/emails/broadcasts/index.blade.php
+++ resources/views/livewire/backend/admin/emails/broadcasts/index.blade.php
@@ -82,10 +82,7 @@
                         <td class="px-4 py-3 text-right">
                             <x-admin.table-action-button
                                 type="link"
-                                :href="route('admin.emails.broadcasts.view', [
-                                        'email_send' => $email_send->id,
-                                        'event' => $event->id,
-                                    ])"
+                                :href="route('admin.dashboard')"
                                 icon="arrow-right-circle"
                                 label="View details"
                                 primary />
