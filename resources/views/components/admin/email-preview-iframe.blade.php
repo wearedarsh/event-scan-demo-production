7fde7c29a35ff4a05d5eb0a9a1c65b319a24fd13
Tom Harley <tomharley@macbookair.home>
2025-12-14 09:53:20 +0000

    updated iframe size

--- resources/views/components/admin/email-preview-iframe.blade.php
+++ resources/views/components/admin/email-preview-iframe.blade.php
@@ -8,24 +8,6 @@
     id="{{ $iframeId }}"
     class="w-full border rounded"
     sandbox
-    style="min-height: 400px; width: 100%; border: 1px solid #ccc;"
+    style="min-height: 500px; width: 100%; border: 1px solid #ccc;"
     src="{{ route('admin.emails.preview', ['email_send' => $id]) }}"
 ></iframe>
-
-@once
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    function resizeIframe(iframe) {
-        try {
-            iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-        } catch(e) {
-            console.warn('Cannot resize iframe', e);
-        }
-    }
-
-    document.querySelectorAll('iframe[id^="email-preview-"]').forEach(iframe => {
-        iframe.onload = () => resizeIframe(iframe);
-    });
-});
-</script>
-@endonce
