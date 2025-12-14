819341358ff97be37f78693fc03da0ee72ef12a4
Tom Harley <tomharley@macbookair.home>
2025-12-14 12:20:16 +0000

    remove links from email preview

--- resources/views/livewire/backend/admin/emails/preview.blade.php
+++ resources/views/livewire/backend/admin/emails/preview.blade.php
@@ -1,1 +1,10 @@
-{!! $html_content !!}
+<div id="iframe-content">{!! $html_content !!}</div>
+
+<style>
+#iframe-content a {
+    pointer-events: none;
+    color: inherit;
+    text-decoration: underline;
+    cursor: default;
+}
+</style>
