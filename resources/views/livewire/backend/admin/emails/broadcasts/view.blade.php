8f3984ec519b6f9f9ec1f160b063b2a4b5745d10
Tom Harley <tomharley@macbookair.home>
2025-12-14 10:16:40 +0000

    updated icons again

--- resources/views/livewire/backend/admin/emails/broadcasts/view.blade.php
+++ resources/views/livewire/backend/admin/emails/broadcasts/view.blade.php
@@ -107,9 +107,9 @@
         <x-admin.tile-card hover="false" class="p-6 space-y-4"
             title="subject"
             :description="$email_send->subject"
-            icon="heroicon-o-pencil-square"
+            icon="heroicon-o-paper-airplane"
         >
-        <x-admin.section-title-icon title="Email content" icon="hero-o-code-pencil-square" />
+        <x-admin.section-title-icon title="Email content" icon="hero-o-paper-airplane" />
             <div class="prose max-w-none">
                 <x-admin.email-preview-iframe :id="$email_send->id" />
             </div>
