ab0a574962f549b5bf479b17c96ac2f28617c34a
Tom Harley <tomharley@macbookair.home>
2026-01-15 11:47:46 +0000

    Updated spelling mistake in approval registration complete email

--- resources/views/emails/customer/approval-registration-complete-confirmation.blade.php
+++ resources/views/emails/customer/approval-registration-complete-confirmation.blade.php
@@ -6,7 +6,7 @@
     <strong>{{ $registration->event->location }}</strong>.
 </p>
 
-<p>A member of our team has received your application and you'll be notified of wether you have been sucessful or not within 3-5 working days.
+<p>A member of our team has received your application and you'll be notified of wether you have been successful or not within 3-5 working days.
 
 <p style="margin-top: 30px;">
     In the mean time if you have any questions, feel free to <a href="mailto:">email us</a>.
