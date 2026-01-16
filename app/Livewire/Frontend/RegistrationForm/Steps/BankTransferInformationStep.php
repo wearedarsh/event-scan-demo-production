8bb260577580aea70660ed9a83cd0a47e402c387
Tom Harley <tomharley@macbookair.home>
2026-01-16 15:53:57 +0000

    Added Registration and registration form models to bank transfer step

--- app/Livewire/Frontend/RegistrationForm/Steps/BankTransferInformationStep.php
+++ app/Livewire/Frontend/RegistrationForm/Steps/BankTransferInformationStep.php
@@ -3,13 +3,18 @@
 namespace App\Livewire\Frontend\RegistrationForm\Steps;
 
 use App\Models\Event;
+use App\Models\RegistrationForm;
+use App\Models\Registration;
 
 use Livewire\Component;
 
 class BankTransferInformation extends Component
 {
+    public Registration $registration;
+    public RegistrationForm $registration_form;
     public Event $event;
     public $step_help_info;
+    
 
     public function render()
     {
