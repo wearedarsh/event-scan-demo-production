9c1e323dc25684f1ea1cf341cd60b339d8243e98
Tom Harley <tomharley@macbookair.home>
2026-01-17 09:56:55 +0000

    Added variable in dump in tickets step blade

--- resources/views/livewire/frontend/registration-form/steps/tickets.blade.php
+++ resources/views/livewire/frontend/registration-form/steps/tickets.blade.php
@@ -105,7 +105,7 @@
         @endforeach
 
         <div class="flex justify-between pt-4">
-            <strong>Total</strong>
+            <strong>Total {{ $registration_total_cents }}</strong>
             <strong>{{ $currency_symbol }}{{ $this->registrationTotal }}</strong>
         </div>
 
